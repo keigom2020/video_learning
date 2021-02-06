169fe5e910058749f1a86a274eb6a0c2632f7c06
km <km@kmnoMacBook-Pro.local>
2021-02-06 16:07:39 +0900

    footerのパーツを追加し、スタイルを記述

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+  <div class="container pt-4">
+    <div class="row">
+      <ul class="mr-5">
+        <li>
+          <a href="">運営会社</a>
+        </li>
+        <li>
+          <a href="">プライバシーポリシー</a>
+        </li>
+      </ul>
+
+      <ul class="">
+        <li>
+          <a href="">運営会社</a>
+        </li>
+        <li>
+          <a href="">プライバシーポリシー</a>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+  <div class="container">
+    <div class="row copyright">
+      <small class="mx-auto">&copy; VideoLearning 2021</small>
+    </div>
+  </div>
+</footer>
