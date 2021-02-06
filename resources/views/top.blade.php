fef045b1a7b8128abfb29fe4c52c94df3e77f588
km <km@kmnoMacBook-Pro.local>
2021-02-06 13:42:29 +0900

    トップページ変更

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,7 +3,26 @@
 @section('content')
 
 <div class="container">
-  <h2>トップページ</h2>
+  <div class="row">
+    <h2 class="mx-auto">Video Learning</h2>
+  </div>
+  <div class="row">
+    <h3 class="mx-auto text-center">動画で学習する<br>プラットフォーム。</h3>
+  </div>
+  
+  <section class="mt-4">
+    <div class="row mx-auto text-center">
+      <a href="/register" class="btn btn-info mx-auto">
+          新規登録する
+      </a>
+    </div>
+
+    <div class="row mx-auto text-center  mt-3">
+      <a href="/login" class="btn btn-info mx-auto">
+          ログインする
+      </a>
+    </div>
+  </section>
 </div>
 
 @endsection('content')
