eaa8dbd87e9aa3c8e3e44ef4747127d068747311
km <km@kmnoMacBook-Pro.local>
2021-02-06 16:07:54 +0900

    不要なナビを削除

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -60,17 +60,4 @@
   </div>
 </nav>
 
-<div class="container pt-4">
-  <div class="ul row">
-    <p>
-      <a href="/course/insurance">保険</a>
-    </p>
-    <p>
-      <a href="/course/realestate">不動産</a>
-    </p>
-    <p>
-      <a href="/course/stock">証券投資</a>
-    </p>
-  </div>
-</div>
 
