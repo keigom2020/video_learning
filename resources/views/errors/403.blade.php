b76bfc17f7e7d7230082b20abe4be6e2236e7da5
km <changem2mind@gmail.com>
2021-02-11 17:07:49 +0900

    参考にしたURLを追加、403のエラーページを追加

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('errors.layouts.base')
+
+@section('title', '403 Forbidden')
+
+@section('message', 'あなたにはアクセス権がありません。')
+{{-- You do not have access. --}}
+
+
+@section('detail', 'クライアントがコンテンツへのアクセス権を持たず、サーバーが適切な応答への返信を拒否していることを示します。')
+{{-- It indicates that the client does not have access to the content and the server is refusing to reply the appropriate response. --}}
+{{-- https://www.ritolab.com/entry/57 --}}
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,6 +8,7 @@
 
 @section('detail', 'サーバーは要求されたリソースを見つけることができなかったことを示します。URLのタイプミス、もしくはページが移動または削除された可能性があります。トップページに戻るか、もう一度検索してください。')
 {{-- サーバーは要求されたリソースを見つけることができなかったことを示します。 The server indicates that it could not find the requested resource. A typo in the URL, or the page may have been moved or deleted. Please go back to the top page or search again. --}}
+{{-- https://www.ritolab.com/entry/57 --}}
 
 @section('link')
   <p><a href="{{env('APP_URL')}}">to TOP&gt;&gt;</a></p>
