5829aebb3ef82942d02b6f2f68f58329f8bafbfe
km <changem2mind@gmail.com>
2021-02-11 16:55:35 +0900

    アンケートページのリンクを追加

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -13,8 +13,13 @@
     <li class="mr-2">
       <a href={{ route('realestate') }}>不動産コース</a>
     </li>
-    <li class="">
+    <li class="mr-2">
       <a href={{ route('stock') }}>証券投資コース</a>
     </li>
+
+    <li class="">
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdptWokRvN-CRv-GjtcmTZExpBzozE-2HS8Lh7xV5Odv7Jk1A/viewform?vc=0&c=0&w=1&flr=0&gxids=7628" target="_blank" rel="noopener">ワンポイントレッスン申し込み</a>
+    </li>
+    
   </ul>
 </nav>
