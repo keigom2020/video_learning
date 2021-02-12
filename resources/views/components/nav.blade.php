8a98c350359d6a0c18e302f9935c7915fe30e06b
km <changem2mind@gmail.com>
2021-02-12 15:56:26 +0900

    ナビゲーションに動画一覧用のページを管理者限定で表示されるようにし追加

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -20,6 +20,12 @@
     <!-- <li class="">
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSdptWokRvN-CRv-GjtcmTZExpBzozE-2HS8Lh7xV5Odv7Jk1A/viewform?vc=0&c=0&w=1&flr=0&gxids=7628" target="_blank" rel="noopener">ワンポイントレッスン申し込み</a>
     </li> -->
+
+    @can('system-only')
+    <li class="mr-2">
+      <a href={{ route('video.index') }}>動画一覧・編集画面</a>
+    </li>
+    @endcan
     
   </ul>
 </nav>
