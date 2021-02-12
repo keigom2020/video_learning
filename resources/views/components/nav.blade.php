5d77ec032b6ac511d3925ef57b46fc4ae31c2809
km <changem2mind@gmail.com>
2021-02-12 14:43:19 +0900

    不必要なリンクを非表示

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -17,9 +17,9 @@
       <a href={{ route('stock') }}>証券投資コース</a>
     </li>
 
-    <li class="">
+    <!-- <li class="">
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSdptWokRvN-CRv-GjtcmTZExpBzozE-2HS8Lh7xV5Odv7Jk1A/viewform?vc=0&c=0&w=1&flr=0&gxids=7628" target="_blank" rel="noopener">ワンポイントレッスン申し込み</a>
-    </li>
+    </li> -->
     
   </ul>
 </nav>
