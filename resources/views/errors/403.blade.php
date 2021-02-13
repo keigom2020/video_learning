e3fb68deeb09dcd9a4537a9d1ea28910422e75cd
km <changem2mind@gmail.com>
2021-02-13 11:59:20 +0900

    トップへ戻るリンクを追加

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -9,3 +9,8 @@
 @section('detail', 'クライアントがコンテンツへのアクセス権を持たず、サーバーが適切な応答への返信を拒否していることを示します。')
 {{-- It indicates that the client does not have access to the content and the server is refusing to reply the appropriate response. --}}
 {{-- https://www.ritolab.com/entry/57 --}}
+
+@section('link')
+  <!-- <p><a href="{{env('APP_URL')}}">to TOP&gt;&gt;</a></p> -->
+  <p><a href="/">to TOP&gt;&gt;</a></p>
+@endsection
