96d2a013976ee2f7868e900b0b31cdbecef14189
km <changem2mind@gmail.com>
2021-02-19 15:28:00 +0900

    視聴履歴用のmodel, controllerを追加

---
+++ app/Http/Controllers/BrowsingHistoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BrowsingHistory;
+use Illuminate\Http\Request;
+
+class BrowsingHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BrowsingHistory  $browsingHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BrowsingHistory $browsingHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BrowsingHistory  $browsingHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BrowsingHistory $browsingHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BrowsingHistory  $browsingHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BrowsingHistory $browsingHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BrowsingHistory  $browsingHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BrowsingHistory $browsingHistory)
+    {
+        //
+    }
+}
