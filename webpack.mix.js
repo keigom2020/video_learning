9d02e14d38fdc6826003129ad79cf9cd008c595d
km <changem2mind@gmail.com>
2021-02-20 16:36:03 +0900

    versionの記述を追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,8 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/ui.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css', {
       implementation: require('dart-sass')
-    });
+    })
+    .version()
 
 mix.browserSync({
     files: [
