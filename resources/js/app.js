43b54cb71c70af420a4f24d7efabefb6ba18dc28
km <changem2mind@gmail.com>
2021-02-21 11:49:39 +0900

    Vueのコンポーネント登録、修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+
+import './bootstrap'
+import Vue from 'vue'
+import BrowsingHistory from './components/BrowsingHistory'
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    BrowsingHistory,
+  }
+})
