c44c1d451efea6d0439923986b796ebbf30942b9
km <changem2mind@gmail.com>
2021-02-22 22:38:40 +0900

    userのマイグレ0ションファイルを修正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,16 +14,23 @@ class UsersTableSeeder extends Seeder
         {
             DB::table('users')->insert([
                 [
-                    'id' => 2,
+                    'id' => 1,
                     'name' => 'test',
                     'email' => 'test@gmail.com',
+                    'password' => 'testtest',
+                    'role' => 1,
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'test2',
+                    'email' => 'test2@gmail.com',
                     'password' => 'testtest',
                     'role' => 4,
                 ],
                 [
                     'id' => 3,
-                    'name' => 'test2',
-                    'email' => 'test2@gmail.com',
+                    'name' => 'test3',
+                    'email' => 'test3@gmail.com',
                     'password' => 'testtest',
                     'role' => 4,
                 ],
