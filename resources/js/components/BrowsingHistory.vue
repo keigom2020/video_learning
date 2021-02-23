0dd3ebcad12b94ce23862e5cac2295bcf7ab458f
km <changem2mind@gmail.com>
2021-02-23 17:51:47 +0900

    vueにメソッドの処理を記述

--- resources/js/components/BrowsingHistory.vue
+++ resources/js/components/BrowsingHistory.vue
@@ -14,6 +14,7 @@
 
     <div
       class="btn btn-info state-change-btn"
+      @click="stateButton"
     >
       <p>視聴済みにする</p>
     </div>
@@ -22,19 +23,31 @@
 </template>
 
 <script>
-// export default {
-//   name: 'BrowsingHistory',
-//   data() {
-//     return {
-//       isActive: false,
-//     }
-//   },
-//   methods: {
-//     toggleButton() {
-//       this.isActive = !this.isActive;
-//     }
-//   },
-// }
+export default {
+  // name: 'BrowsingHistory',
+  // メソッドの実行
+  // created() {
+  // },
+
+  // メソッドで使う&テンプレート内で使う変数を定義
+  data() {
+    return {
+      state_button:'',
+    }
+  },
+  // (読み込み時に)実行するメソッド
+  methods: {
+    stateButton() {
+      var video = {
+        'state_button': this.state_button,
+      };
+      var id = 1;
+      axios.post('/api/video/' + id,video).then(res => {
+        console.log(res.data.state_button);
+      });
+    }
+  },
+}
 </script>
 
 <style>
