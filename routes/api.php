0e59fe2186b06b5e05c4b5b3d2f1d500734be0b0
km <changem2mind@gmail.com>
2021-02-23 18:02:56 +0900

    記事を参考にしapi.phpをコメントアウトつきで修正

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::group(['middleware' => ['api']], function(){
+//     Route::resource('video.show', 'VideoController');
+//   });
+
+
+// https://qiita.com/Yorinton/items/1fe722ce9ccef3aa1c96
+// Route::group(['middleware' => 'api'],function(){
+//     Route::post('/video/{id}',function($id){
+//         $user = App\User::where('id',$id)->first(); //->firstではないよね？
+
+//         $video = new App\Video();
+//         $video->state_button = request('state_button');
+//         // $video->title = request('title');
+//         // $video->content = request('content');
+
+//         $user->videos()->save($video);
+
+//         return ['state_button' => request('state_button')]; //1つだけでも配列の[]で表示すべきなのか？
+//     });
+
+// });
