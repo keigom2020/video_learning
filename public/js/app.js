41babc919c432d3534d4f298bc283708409439f1
km <changem2mind@gmail.com>
2021-02-26 18:06:25 +0900

    現状コミット

--- public/js/app.js
+++ public/js/app.js
@@ -1952,7 +1952,16 @@ __webpack_require__.r(__webpack_exports__);
       var id = 1;
       axios.post('/api/video/' + id, video).then(function (res) {
         console.log(res.data.state_button);
-      });
+      }); // axios.post('/tweet', {
+      //   tweet: this.tweet
+      // })
+      // .then(function (response) {
+      //   console.log(response);
+      // })
+      // .catch(function (error) {
+      //   console.log(error);
+      // });
+      // https://kumatetsublog.com/shoot/blog/laravel-ajax-vue-axios
     }
   }
 });
@@ -49819,15 +49828,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*!*****************************************************!*\
   !*** ./resources/js/components/BrowsingHistory.vue ***!
   \*****************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _BrowsingHistory_vue_vue_type_template_id_5265189c___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./BrowsingHistory.vue?vue&type=template&id=5265189c& */ "./resources/js/components/BrowsingHistory.vue?vue&type=template&id=5265189c&");
 /* harmony import */ var _BrowsingHistory_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./BrowsingHistory.vue?vue&type=script&lang=js& */ "./resources/js/components/BrowsingHistory.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _BrowsingHistory_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _BrowsingHistory_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -49857,7 +49865,7 @@ component.options.__file = "resources/js/components/BrowsingHistory.vue"
 /*!******************************************************************************!*\
   !*** ./resources/js/components/BrowsingHistory.vue?vue&type=script&lang=js& ***!
   \******************************************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
