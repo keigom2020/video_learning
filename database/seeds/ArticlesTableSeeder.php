0943561f2b355d4e7b286f7399bddd9fc2eb3d44
km <changem2mind@gmail.com>
2021-02-26 19:27:18 +0900

    seederファイルを修正

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -15,19 +15,24 @@ class ArticlesTableSeeder extends Seeder
         //
         DB::table('Articles')->insert([
             [
+                "id" => 1,
+                "title" => "test_title",
+                "content" => "test_content1",
+            ],
+            [
                 "id" => 2,
                 "title" => "test_title",
-                "content" => "test_content",
+                "content" => "test_content2",
             ],
             [
                 "id" => 3,
                 "title" => "test_title",
-                "content" => "test_content",
+                "content" => "test_content3",
             ],
             [
                 "id" => 4,
                 "title" => "test_title",
-                "content" => "test_content",
+                "content" => "test_content4",
             ],
         ]);
     }
