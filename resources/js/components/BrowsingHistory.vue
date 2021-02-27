2cfa0555c4de21bf24d6d17e3a8f921c4cdd4b46
km <changem2mind@gmail.com>
2021-02-27 23:09:54 +0900

    不要な物を削除

--- resources/js/components/BrowsingHistory.vue
+++ resources/js/components/BrowsingHistory.vue
@@ -3,14 +3,6 @@
     
   </div> -->
   <div class="mx-auto">
-    <!-- <button
-      type="button"
-      class="btn m-0 p-1 shadow-none"
-    >
-      <i class="fas fa-heart mr-1"
-      />
-    </button>
-    10 -->
 
     <div
       class="btn btn-info state-change-btn"
