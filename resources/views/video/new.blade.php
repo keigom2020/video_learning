e50348c97a0bd03953dc94211d3717c254bfd029
km <changem2mind@gmail.com>
2021-02-27 15:09:02 +0900

    動画の新規投稿用のviewを追加

---
+++ resources/views/video/new.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+  <h2>動画</h2>
+  <p>{{ $message }}</p>
+</div>
+
+@can('system-only')
+
+@foreach ($videos as $video)
+  
+  <div class="container">
+    <p>
+
+      <a href='{{ route("video.show", ["id" =>  $video->id]) }}'>
+        {{ $video->id }}:
+        {{ $video->title }}
+      </a>
+
+    <a href={{ route('video.delete') }}>削除</a>
+
+    </p>
+  </div>
+
+@endforeach
+
+<div class="container">
+  <div>
+    <a href={{ route('video.new') }}>新規投稿</a>
+  </div>
+</div>
+
+
+@endcan
+
+@endsection
