0079381532ceb042e7595547110e3e10cf8a201b
km <changem2mind@gmail.com>
2021-02-27 19:41:27 +0900

    usersのseederを変更

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,24 +15,31 @@ class UsersTableSeeder extends Seeder
             DB::table('users')->insert([
                 [
                     'id' => 1,
-                    'name' => 'test',
+                    'name' => 'guest',
                     'email' => 'test@gmail.com',
                     'password' => 'testtest',
-                    'role' => 1,
+                    'role' => 4,
                 ],
                 [
                     'id' => 2,
                     'name' => 'test2',
                     'email' => 'test2@gmail.com',
-                    'password' => 'testtest',
+                    'password' => '$2y$10$jasut87Ff/UtAK.tv7Tdee0.oKU7bOMoFsqmZNaebaLAMq8VOXyT2',
                     'role' => 4,
                 ],
                 [
                     'id' => 3,
                     'name' => 'test3',
                     'email' => 'test3@gmail.com',
-                    'password' => 'testtest',
+                    'password' => '$2y$10$jasut87Ff/UtAK.tv7Tdee0.oKU7bOMoFsqmZNaebaLAMq8VOXyT2',
                     'role' => 4,
+                ],
+                [
+                    'id' => 99,
+                    'name' => 'Administrator',
+                    'email' => 'administrator@gmail.com',
+                    'password' => '$2y$10$jasut87Ff/UtAK.tv7Tdee0.oKU7bOMoFsqmZNaebaLAMq8VOXyT2',
+                    'role' => 1,
                 ],
             ]);
         }
