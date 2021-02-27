64ed6a85cd35788323b08bd76836f9f90e33e1db
km <changem2mind@gmail.com>
2021-02-27 17:28:40 +0900

    動画用の新規投稿ページのフォーム項目を追加

--- resources/views/video/new.blade.php
+++ resources/views/video/new.blade.php
@@ -5,16 +5,42 @@
 <div class="container">
 
     <h1>新規投稿</h1>
+    
     <p>{{ $message }}</p>
     {{ Form::open(['route' => 'video.store']) }}
         <div class='form-group'>
-            {{ Form::label('user_name', 'タイトル:') }}
-            {{ Form::text('title') }}
-            {{-- {{ Form::text('title', null) }} --}}
+            {{ Form::label('title', 'タイトル:') }}
+            {{ Form::text('title', 'test') }}
         </div>
+
+        <div class='form-group'>
+            {{ Form::label('video_url', 'video_url(vimeoの動画IDを入力してください):') }}
+            {{ Form::text('video_url', '502084279') }}
+        </div>
+
+        <div class='form-group'>
+            {{ Form::label('image_src', 'image_src(画像名を入力):') }}
+            {{ Form::text('image_src', 'default.jpg') }}
+        </div>
+
+        <div class='form-group'>
+            {{ Form::label('detail', 'detail:') }}
+            {{ Form::text('detail', 'testtest') }}
+        </div>
+
+        <div class='form-group'>
+            {{ Form::label('author', 'author:') }}
+            {{ Form::text('author', '小西太郎') }}
+        </div>
+
+        <div class='form-group'>
+            {{ Form::label('category_id', 'category_id:') }}
+            {{ Form::text('category_id', 3) }}
+        </div>
+
         <div class='form-group'>
             {{ Form::label('content', '内容:') }}
-             {{ Form::text('content', null) }}
+            {{ Form::textarea('content', 'testtest') }}
         </div>
         
         <div class="form-group">
